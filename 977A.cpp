b57654a7fc90e2d9d85bca6032bab6b921b9065c
niteesh1407 <117834365+Niteesh1407@users.noreply.github.com>
2022-12-29 23:31:14 +0530

    Create 977A.cpp

---
+++ 977A.cpp
@@ -0,0 +1,21 @@
+//link:  https://codeforces.com/problemset/problem/977/A
+
+CODE:
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    while(k--)
+    {
+        if(n%10==0)
+        {
+            n=n/10;
+        }
+        else
+        n=n-1;
+    }
+    cout<<n;
+}
