95e26c27c2a2634378d32c0c8a0c90747271f346
niteesh1407 <117834365+Niteesh1407@users.noreply.github.com>
2022-12-29 23:23:39 +0530

    Create 263A.cpp

---
+++ 263A.cpp
@@ -0,0 +1,22 @@
+//link: https://codeforces.com/problemset/problem/263/A
+
+CODE:
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[5][5];
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+           cin>>a[i][j];
+           if(a[i][j]==1)
+           {
+               cout<<abs(i-2)+abs(j-2);
+               exit(0);
+           }
+        }
+    }
+}
